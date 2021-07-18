d69953ca5868bf1750542112074ea27139d078cc
Andrey Bulanov <bulanov1804@gmail.com>
2021-07-18 20:49:50 +0300

    Задание на проверку

---
+++ src/fetchCountries.js
@@ -0,0 +1,8 @@
+const URL = 'https://restcountries.eu/rest/v2';
+
+function fetchCountries (name) {
+    return fetch(`${URL}/name/${name}`).then(response => response.json());    
+}
+
+export default {fetchCountries};
+
---
+++ src/getRefs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+    return {
+    countryInfo: document.querySelector('.country-info'),
+    countryList: document.querySelector('.country-list'),
+    searchCountry: document.querySelector('#search-box'),
+  }
+}
